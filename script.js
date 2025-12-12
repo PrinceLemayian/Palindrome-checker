06083178cfeeccb94624f154a0c2ef9c87c550dd
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-12-13 00:50:23 +0300

    Initial commit for palindrome checker

---
+++ script.js
@@ -0,0 +1,18 @@
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result")
+
+checkButton.addEventListener("click", () => {
+  const replaced = textInput.value.replace(/[^A-Za-z0-9]/g,"").toLowerCase();
+  if ((textInput.value) === "") {
+    alert("Please input a value");
+  } else if (textInput.value.length === 1) {
+    result.innerText = `${textInput.value} is a palindrome`
+  } else if (replaced === [...replaced].reverse().join("")) {
+    result.innerText = `${textInput.value} is a palindrome`
+   } else {
+    result.innerText = `${textInput.value} is not a palindrome`
+   }
+});
+
+
